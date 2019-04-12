fc9fdec386b022a412002c4dffba9e233f480d73
Nipun Sampath <nipunsampath@gmail.com>
2019-04-13 01:01:52 +0530

    added error and sucess feedbacks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from FolderOperations import FolderOperations
 from GUI.mainUI import Ui_MainWindow
-from PyQt5 import QtWidgets,QtGui
+from PyQt5 import QtWidgets, QtGui
 
 import sys
 
@@ -16,18 +16,53 @@ class Main(QtWidgets.QMainWindow):
 
         self.ui.openFolderPath.clicked.connect(self.openFolderSelectDialog)
         self.ui.createSeries.clicked.connect(self.createSeriesDirectory)
+        self.onlyInt = QtGui.QIntValidator()
+        # self.ui.numberOfEpisodes.textChanged.connect(self.check_state)
+        # self.ui.numberOfEpisodes.emit(self.ui.numberOfEpisodes.text())
+        self.ui.numberOfEpisodes.setValidator(self.onlyInt)
+        self.ui.numberOfSeasons.setValidator(self.onlyInt)
 
     def openFolderSelectDialog(self):
-        fileName = QtWidgets.QFileDialog.getExistingDirectory(None, "Select Path","", QtWidgets.QFileDialog.ShowDirsOnly)
+        fileName = QtWidgets.QFileDialog.getExistingDirectory(None, "Select Path", "",
+                                                              QtWidgets.QFileDialog.ShowDirsOnly)
         self.ui.folderPathLineEdit.setText(fileName)
 
     def createSeriesDirectory(self):
         path = self.ui.folderPathLineEdit.text()
         seriesName = self.ui.seriesName.text()
-        numberOfSeasons = self.ui.numberOfSeasons.text()
-        numberOfEpisodes = self.ui.numberOfEpisodes.text()
-        folderOps = FolderOperations(seriesName,numberOfSeasons,numberOfEpisodes,path)
-        folderOps.make_series()
+        numberOfSeasons = int(self.ui.numberOfSeasons.text())
+        numberOfEpisodes = int(self.ui.numberOfEpisodes.text())
+
+        folderOps = FolderOperations(seriesName, numberOfSeasons, numberOfEpisodes, path)
+        requestCode = folderOps.make_series()
+
+        msgBox = QtWidgets.QMessageBox()
+        if requestCode == FolderOperations.SUCCESS:
+            msgBox.setIcon(QtWidgets.QMessageBox.Information)
+            msgBox.setText('Directories Created!')
+            msgBox.setWindowTitle("Success")
+            msgBox.exec_()
+        else:
+            msgBox.setIcon(QtWidgets.QMessageBox.Critical)
+
+            msgBox.setIcon(QtWidgets.QMessageBox.Critical)
+            msgBox.setText('Directory already Exists')
+            msgBox.setWindowTitle("Error")
+            msgBox.exec_()
+
+    # To DO
+    # def check_state(self, *args, **kwargs):
+    #     sender = self.sender()
+    #     validator = sender.validator()
+    #     state = validator.validate(sender.text(), 0)[0]
+    #     if state == QtGui.QValidator.Acceptable:
+    #         color = '#c4df9b'  # green
+    #     elif state == QtGui.QValidator.Intermediate:
+    #         color = '#fff79a'  # yellow
+    #     else:
+    #         color = '#f6989d'  # red
+    #     sender.setStyleSheet('QLineEdit { background-color: %s }' % color)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
