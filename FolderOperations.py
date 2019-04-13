11bc1af87647ff020b028aee5301504d4ce306dc
Nipun Sampath <nipunsampath@gmail.com>
2019-04-14 01:55:33 +0530

    changed maikeSeries to return request codes

--- FolderOperations.py
+++ FolderOperations.py
@@ -2,7 +2,6 @@ import os
 
 
 class FolderOperations:
-
     SUCCESS = 100
     OSERROR = 200
 
@@ -16,7 +15,7 @@ class FolderOperations:
         """Creates a new directory for every episode in the season"""
         try:
             for episode in range(1, self.number_of_episodes + 1):
-                os.mkdir(path + "\\{:02d}".format(episode))
+                os.mkdir(path + "/{:02d}".format(episode))
         except OSError as e:
             print(str(e))
 
@@ -25,7 +24,7 @@ class FolderOperations:
         season_paths = []
         try:
             for season in range(1, self.number_of_seasons + 1):
-                season_path = path + "\\Season %2d" % season
+                season_path = path + "/Season %2d" % season
                 os.mkdir(season_path)
                 season_paths.append(season_path)
             return season_paths
@@ -34,7 +33,7 @@ class FolderOperations:
 
     def make_series(self):
         """Creates a directory for the tv series with a directory for every episode"""
-        series_path = self.path + "\\" + self.series_name
+        series_path = self.path + "/" + self.series_name
         try:
             os.mkdir(series_path)
             paths = self.make_seasons(series_path)
@@ -48,21 +47,24 @@ class FolderOperations:
     def make_series_without_episode_dir(self):
         """Creates a directory for the tv series with directories for seasons and a subtitle directory per each
         season """
-        series_path = self.path + "\\" + self.series_name
+        series_path = self.path + "/" + self.series_name
         try:
             os.mkdir(series_path)
             paths = self.make_seasons(series_path)
             for path in paths:
-                os.mkdir(path + "\\Subtitles")
+                os.mkdir(path + "/Subtitles")
+
+            return FolderOperations.SUCCESS
         except OSError as e:
             print(str(e))
+            return FolderOperations.OSERROR
 
 
 if __name__ == "__main__":
-    series_name = "Make Human2"
+    series_name = "Make Human23"
     number_of_seasons = 2
     number_of_episodes = 20
-    folder_path = "D:\\Projects\\Python\\Folder Maker\\test"
+    folder_path = "D:/Projects/Python/Folder Maker/test"
 
     driver = FolderOperations(series_name, number_of_seasons, number_of_episodes, folder_path)
     driver.make_series_without_episode_dir()
