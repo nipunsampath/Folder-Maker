908f2cf99b9c07dc7bdc7d9867dcc9420e3bfaaa
Nipun Sampath <nipunsampath@gmail.com>
2019-04-14 16:52:35 +0530

    - added create movies method - added logging

--- FolderOperations.py
+++ FolderOperations.py
@@ -1,4 +1,7 @@
 import os
+import logging
+
+logging.basicConfig(filename="log_file.log", level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s")
 
 
 class FolderOperations:
@@ -7,29 +10,31 @@ class FolderOperations:
 
     def __init__(self, name, number_of_seasons, number_of_episodes, path):
         self.series_name = name
-        self.number_of_episodes = number_of_episodes
-        self.number_of_seasons = number_of_seasons
+        self.numberOfEpisodes = number_of_episodes
+        self.numberOfSeasons = number_of_seasons
         self.path = path
 
     def make_episodes(self, path):
         """Creates a new directory for every episode in the season"""
         try:
-            for episode in range(1, self.number_of_episodes + 1):
+            for episode in range(1, self.numberOfEpisodes + 1):
                 os.mkdir(path + "/{:02d}".format(episode))
         except OSError as e:
-            print(str(e))
+            logging.debug(str(e))
+            raise OSError
 
     def make_seasons(self, path):
         """Creates a new directory for every season """
         season_paths = []
         try:
-            for season in range(1, self.number_of_seasons + 1):
+            for season in range(1, self.numberOfSeasons + 1):
                 season_path = path + "/Season %2d" % season
                 os.mkdir(season_path)
                 season_paths.append(season_path)
             return season_paths
         except OSError as e:
-            print(str(e))
+            logging.debug(str(e))
+            raise OSError
 
     def make_series(self):
         """Creates a directory for the tv series with a directory for every episode"""
@@ -41,7 +46,7 @@ class FolderOperations:
                 self.make_episodes(path)
             return FolderOperations.SUCCESS
         except OSError as e:
-            print(str(e))
+            logging.debug(str(e))
             return FolderOperations.OSERROR
 
     def make_series_without_episode_dir(self):
@@ -52,11 +57,31 @@ class FolderOperations:
             os.mkdir(series_path)
             paths = self.make_seasons(series_path)
             for path in paths:
-                os.mkdir(path + "/Subtitles")
+                self.make_subtitles_directory(path)
 
             return FolderOperations.SUCCESS
         except OSError as e:
-            print(str(e))
+            logging.debug(str(e))
+            return FolderOperations.OSERROR
+
+    def make_subtitles_directory(self, path):
+        try:
+            os.mkdir(path + "/Subtitles")
+        except OSError as e:
+            logging.debug(str(e))
+            raise OSError
+
+    def make_movie_directories(self, movies, need_subs):
+        try:
+            for movie in movies:
+                movie_path = self.path + "/" + movie
+                os.mkdir(movie_path)
+
+                if need_subs:
+                    self.make_subtitles_directory(movie_path)
+            return FolderOperations.SUCCESS
+        except OSError as e:
+            logging.debug(str(e))
             return FolderOperations.OSERROR
 
 
@@ -65,6 +90,7 @@ if __name__ == "__main__":
     number_of_seasons = 2
     number_of_episodes = 20
     folder_path = "D:/Projects/Python/Folder Maker/test"
+    movies = ["Avengers", "Doctor who", "Pacific Rim"]
 
-    driver = FolderOperations(series_name, number_of_seasons, number_of_episodes, folder_path)
-    driver.make_series_without_episode_dir()
+    driver = FolderOperations(None, None, None, folder_path)
+    driver.make_movie_directories(movies, True)
